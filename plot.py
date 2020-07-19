321bb1724b42f1c3a68c054996601cbea461c6ce
Yiwen Wang <yiwenwang9702@gmail.com>
2020-07-19 00:34:55 -0400

    minor

--- plot.py
+++ plot.py
@@ -2,9 +2,12 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 def plot_3d(mol):
+
     """
     A very simple way to plot a molecule in a 3d fashion. For the sake of visualizing bonds.
+    Type hint is omitted to avoid repeated imports.
     """
+    
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(xs=mol.coords_all[:, 0],ys=mol.coords_all[:, 1],zs=mol.coords_all[:, 2],s=50)
